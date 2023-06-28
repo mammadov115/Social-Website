5ce7c00acb1e30ac9df72bad77e430705a49c463
Mammadov <mehmanmehman182@gmail.com>
2023-06-28 18:00:31 +0400

    Adding asynchronous actions with JavaScript

--- images/forms.py
+++ images/forms.py
@@ -12,25 +12,23 @@ class ImageCreateForm(forms.ModelForm):
             'url':forms.HiddenInput
         }
 
-        def clean_url(self):
-            url = self.cleaned_data['url']
-            valid_extensions = ['jpg','jpeg','png']
-            extension = url.rsplit('.',1)[1].lower()
-            if extension not in valid_extensions:
-                raise forms.ValidationError('The given URL does not match valid image ')
-            return url
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg','jpeg','png']
+        extension = url.rsplit('.',1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image ')
+        return url
         
-        def save(self,force_insert=False,force_update=False,commit=True):
-            image = super().save(commit=False)
-            image_url = self.cleaned_data['url']
-            name = slugify(image.title)
-            extension = image_url.rsplit('.', 1)[1].lower()
-            image_name = f'{name}.{extension}'
-            # download image from the given URL
-            response = requests.get(image_url)
-            image.image.save(image_name,
-                            ContentFile(response.content),
-                            save=False)
-            if commit:
-                image.save()
-            return image
+    def save(self,force_insert=False,force_update=False,commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name,ContentFile(response.content),save=False)
+        if commit:
+            image.save()
+        return image
--- images/views.py
+++ images/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+
 
 # Create your views here.
 
@@ -12,10 +18,11 @@ def image_create(request):
         if form.is_valid():
             # form data is valid
             cd = form.cleaned_data
+            print(cd)
             new_image = form.save(commit=False)
             # assign current user to the item
             new_image.user = request.user
-            new_image.save()
+            print(new_image.save())
             messages.success(request, 'Image added successfully')
             # redirect to new created image detail view
             return redirect(new_image.get_absolute_url())
@@ -28,3 +35,30 @@ def image_create(request):
     }
 
     return render(request,'images/image/create.html',context)
+
+def image_detail(request,id,slug):
+    image = get_object_or_404(Image,id=id,slug=slug)
+    context = {
+        'secion':'images',
+        'image':image
+    }
+
+    return render(request,'images/image/detail.html',context)
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status':'error'})
