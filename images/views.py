b9b8c7019d836975404f7358a2c8ae569ba3c639
Mammadov <mehmanmehman182@gmail.com>
2023-07-02 21:28:14 +0400

    m

--- images/views.py
+++ images/views.py
@@ -4,10 +4,9 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from django.shortcuts import get_object_or_404
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.views.decorators.http import require_POST
-
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -62,3 +61,29 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status':'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images,8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    context = {
+        'section':'images',
+        'images':images
+    }
+
+    if images_only:
+        return render(request,'images/image/list_images.html',context)
+
+    return render(request,'images/image/list.html',context)
